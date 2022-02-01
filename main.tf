10df5ddb55966b8164419907c0ba4ed74f60b305
achavan655 <54505652+achavan655@users.noreply.github.com>
2022-02-01 11:13:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,12 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
-  
-
-  name = "single-new-instance"
-
-  ami                    = "ami-0a8b4cd432b1c3063"
-  instance_type          = "t2.micro"
-  key_name               = "A_jenkins"
-  
-  
+resource "aws_instance" "Demo" {
+  ami           = "ami-0a8b4cd432b1c3063"
+  instance_type = "t2.micro"
+  key_name = "A_jenkins"
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+    key = "Schedule"
+    Name = "Bangalore-office-hours"
   }
 }
